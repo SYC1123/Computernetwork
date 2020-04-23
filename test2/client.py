68051f1361542e0ffd3d671809b083bf7696f6b5
syc1123 <874795069@qq.com>
2020-04-23 22:02:28 +0800

    计网实验

--- test2/client.py
+++ test2/client.py
@@ -2,6 +2,8 @@
 from socket import *
 from time import ctime
 
+Flag = 0
+reSendFlag = False
 if __name__ == '__main__':
     HOST = '127.0.0.1'  # 服务端ip
     PORT = 21566  # 服务端端口号
@@ -10,18 +12,27 @@ if __name__ == '__main__':
     tcpCliSock = socket(AF_INET, SOCK_STREAM)  # 创建socket对象
     tcpCliSock.connect(ADDR)  # 连接服务器
     user_input = ''
-    user_input = input()
-    tcpCliSock.settimeout(3)
+    user_input = input().strip()
+    tcpCliSock.settimeout(0.3)
     while user_input != 'quit':
+        if reSendFlag==0:
+            user_input = str(Flag) + ':' + user_input
+        reSendFlag = False
         try:
             tcpCliSock.sendto(user_input.encode('utf-8'), ADDR)
             # tcpCliSock.send(user_input.encode('utf-8'))
             returnData = tcpCliSock.recv(BUFSIZ)  # 读取消息
-            print(returnData.decode('utf-8'))
+            returnData = int(returnData.decode('utf-8'))
+            if returnData == -1:
+                print("发送了错误帧")
+                reSendFlag = True
+            else:
+                print("信息已经收到，返回确认帧为ACK(%d)" % returnData)
+                Flag = returnData
         except timeout:
-            print("dfsf")
-            # tcpCliSock.close()
-            # reConnnectTCP()
+            print("发生了超时")
+            reSendFlag=True
         finally:
-            user_input = input()
+            if reSendFlag != 1:
+                user_input = input().strip()
     tcpCliSock.close()  # 关闭客户端
